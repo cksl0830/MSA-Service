7f2ae9b3292310443dc0b7fbfe559638f23a6e91
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-10-07 17:03:43 +0900

    apply JpaRepository

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -4,30 +4,40 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-@JsonFilter("UserInfo")
+/**
+ * @JsonProperty : json 형식에서 key 이름 설정
+ * @JsonIgnore : 클라이언트가 보지못하게 정적 필터링
+ * @JsonIgnoreProperties : 클라이언트가 보지못하게 정적 필터링 (전체 클래스에서 복수 설정)
+ * @JsonFilter : 동적 필터링 (url마다 다르게 보여주기)
+ */
+
+//@JsonFilter("UserInfo")
 //@JsonIgnoreProperties("user_name","team")
+@Entity(name = "member")
 public class User {
-    /**
-     * @JsonProperty : json 형식에서 key 이름 설정
-     * @JsonIgnore : 클라이언트가 보지못하게 정적 필터링
-     * @JsonIgnoreProperties : 클라이언트가 보지못하게 정적 필터링 (전체 클래스에서 복수 설정)
-     * @JsonFilter : 동적 필터링 (url마다 다르게 보여주기)
-     */
+
+    @Id
+    @GeneratedValue
     private Integer id;
-    @NotNull
-//    @JsonProperty("user_name")
+
+    //@JsonProperty("user_name")
     @Size(min = 2, message = "2글자 이상 입력하세요.")
     private String name;
-    @NotNull(message = "팀을 입력하세요.")
+
     private String team;
-    @NotNull(message = "생년월일을 입력하세요.")
-//    @JsonIgnore
+
+    //@JsonIgnore
+//    @NotNull(message = "생년월일을 입력하세요.")
     private LocalDate birth;
+
     private String password;
 
     public User(Integer id, String name, String team, LocalDate birth, String password) {
@@ -36,6 +46,9 @@ public class User {
         this.team = team;
         this.birth = birth;
         this.password = password;
+    }
+
+    public User() {
     }
 
     public Integer getId() {
--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import com.rest.webservices.restfulwebservices.exception.UserNotFoundException;
+import com.rest.webservices.restfulwebservices.jpa.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
@@ -9,26 +10,31 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-public class UserController {
+@RequestMapping("/jpa")
+public class UserJpaController {
 
-    private final UserDaoService userDaoService;
+//    private UserDaoService userDaoService;
+    private UserRepository userRepository;
 
-    public UserController(UserDaoService userDaoService) {
-        this.userDaoService = userDaoService;
+    public UserJpaController(UserDaoService userDaoService, UserRepository userRepository) {
+//        this.userDaoService = userDaoService;
+        this.userRepository = userRepository;
     }
 
     // 멤버 전체 조회
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return userDaoService.findAll();
+//        return userDaoService.findAll();
+        return userRepository.findAll();
     }
 
     // 멤버 추가 (상태코드 201 반환 - POST 메소드의 반환값에 URI를 추가)
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = userDaoService.save(user);
+        User savedUser = userRepository.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId())
@@ -39,18 +45,22 @@ public class UserController {
     // 멤버 한명 조회
     @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        User user = userDaoService.findOne(id);
-        if (user == null)
+//        User user = userDaoService.findOne(id);
+//        if (user == null)
+//            throw new UserNotFoundException("id " + id + ": not found");
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty())
             throw new UserNotFoundException("id " + id + ": not found");
 
-        EntityModel<User> entityModel = EntityModel.of(user);
+
+        EntityModel<User> entityModel = EntityModel.of(user.get());
         return entityModel;
     }
 
     // 멤버 삭제
     @DeleteMapping ("/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        userDaoService.deleteById(id);
+        userRepository.deleteById(id);
     }
 
 }
