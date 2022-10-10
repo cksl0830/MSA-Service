e8682f73f19b3112439de33b1e8c5afc12433dcc
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-10-10 16:04:15 +0900

    apply convert

---
+++ microservices/currency-conversion-service/src/test/java/msa/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
